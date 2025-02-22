1c2357e5bc21a5a572e0aa6718e0769d73c54b8b
Lucaswillians <lucaswilianserpa@gmail.com>
2025-02-21 21:08:16 -0300

    fix: duplications at transfers

--- src/services/event-sourcing/event-sourcing.repository.ts
+++ src/services/event-sourcing/event-sourcing.repository.ts
@@ -8,10 +8,11 @@ export class EventSourcingRepository extends Repository<EventSourcingEventEntity
     super(EventSourcingEventEntity, dataSourced.manager);
   }
 
-  async saveEvents (events: EventSourcingEventEntity[], manager?: EntityManager): Promise<EventSourcingEventEntity[]> {
-    if (manager) manager.save(events);
-
-    return this.save(events);
+  async saveEvents(events: EventSourcingEventEntity[], manager?: EntityManager): Promise<EventSourcingEventEntity[]> {
+    if (manager) {
+      return await manager.save(events);
+    }
+    return await this.save(events);
   }
 
   async findEventsForEntity(entityName: string, entityId: number, manager?: EntityManager): Promise<EventSourcingEventEntity[]> {
